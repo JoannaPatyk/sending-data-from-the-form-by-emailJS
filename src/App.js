d0fb9ab1c6a0adf93bc297d876d4f5daba06e8e4
Joanna Patyk <patykjoanna93@gmail.com>
2023-08-09 12:05:00 +0200

    The basic version of the form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 function App() {
-    return <div>APP</div>;
+    return (
+        <div className="container">
+            <form className="contact-form">
+                <h2>FORMULARZ KONTAKTOWY</h2>
+                <input type="text" className="input" placeholder="Imię" />
+                <input type="email" className="input" placeholder="e-mail" />
+                <textarea className="textarea" placeholder="Wiadomość..."></textarea>
+                <div className="agreement">
+                    <label className="form-control">
+                        <input type="checkbox" id="checkbox" name="checkbox" />
+                        <p>
+                            Wyrażam zgodę na przetwarzanie moich danych osobowych zgodnie z ustawą o ochronie danych
+                            osobowych w związku z realizacją zgłoszenia, Polityka prywatności.
+                        </p>
+                    </label>
+                </div>
+                <button className="btn" type="submit">
+                    wyślij
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
